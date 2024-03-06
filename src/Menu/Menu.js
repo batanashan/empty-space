8e411f43983d27bd63f57b7e47f2ba5fe417e9b8
shanmukha <shanmukhaciv@gmail.com>
2024-03-06 17:49:16 +0530

    mobilechanges

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link,Routes,Route} from 'react-router-dom'
 import './Menu.css'
 import Home from '../RoutingComponents/Home/Home'
 import About from '../RoutingComponents/About/About'
 import Contact from '../RoutingComponents/Contact/Contact'
 const Menu = () => {
+  const [isShow,setIsShow] = useState(document.body.clientWidth>700 ?false : true)
+  const [left,setLeft] = useState(-150)
+
+  const fnResize =() =>{
+    if(document.body.clientWidth<700){
+    setIsShow(true)
+    }
+    else{
+      setIsShow(false)
+    }
+  }
+  window.addEventListener('resize',fnResize)
+            const handleMobileBtnClick =() =>{
+         setLeft(left === 0? -150 :0)
+           }
+
+           const fnMenuClick = (eve)=>{
+            eve.stopPropagation()
+            setLeft(-150)
+            
+           }
+
   return (
     <div>
-    <ul className='menu'>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="about">About</Link></li>
+    { isShow && <button className='mobile-btn' onClick={handleMobileBtnClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+        
+
+      </button>
+}
+    <ul style ={{left}}  className={`${isShow? 'mobile-menu' : 'menu' }`}>
+        <li ><Link to="/home" onClick={fnMenuClick}>Home</Link></li>
+        <li><Link to="/contact"  onClick={fnMenuClick}>Contact</Link></li>
+        <li><Link to="about"  onClick={fnMenuClick}>About</Link></li>
     </ul>
 <Routes>
 <Route path="/home" element={<Home/>}></Route>
 <Route path="/contact" element={<Contact/>}></Route>
 <Route path="/about" element={<About/>}></Route>
 </Routes>
-    </div>
+     
+     </div>
   )
 }
 
--- src/RoutingComponents/Contact/Contact.js
+++ src/RoutingComponents/Contact/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <h1 className='text-center fs-5'>
+  return <div>
+   <h1 className='text-center fs-5'>
       Contact..........12345
     </h1>
-  )
+  
+  </div>
 }
 
 export default Contact
